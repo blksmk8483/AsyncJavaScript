c2f147e3174d11a5d724b73dc9d450176e6c5bd9
Brandon Knight <knightbrandon1984@gmail.com>
2024-02-07 15:35:07 -0600

    second call to show neighbouring country

--- script.js
+++ script.js
@@ -5,23 +5,51 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
-const getCountryData = function (data, className = '') {
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v3.1/name/${data}`);
-  request.send();
+// const getCountryData = function (data, className = '') {
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v3.1/name/${data}`);
+//   request.send();
 
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
+//   request.addEventListener('load', function () {
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
 
-    const name = data.name.common;
-    const flag = data.flags.svg;
-    const region = data.region;
-    const language = Object.values(data.languages)[0];
-    const currency = Object.values(data.currencies)[0].name;
-    const symbol = Object.values(data.currencies)[0].symbol;
+//     const name = data.name.common;
+//     const flag = data.flags.svg;
+//     const region = data.region;
+//     const language = Object.values(data.languages)[0];
+//     const currency = Object.values(data.currencies)[0].name;
+//     const symbol = Object.values(data.currencies)[0].symbol;
 
-    const html = `    
+//     const html = `
+//     <article class="country ${className}">
+//     <img class="country__img" src="${flag}" alt=${data.flags.alt}/>
+//     <div class="country__data">
+//       <h3 class="country__name">${name}</h3>
+//       <h4 class="country__region">${region}</h4>
+//       <p class="country__row"><span>👫</span>${(
+//         +data.population / 1000000
+//       ).toFixed(1)} people</p>
+//       <p class="country__row"><span>🗣️</span>${language}</p>
+//       <p class="country__row"><span>💰</span>${currency} ${symbol}</p>
+//     </div>
+//   </article>
+// `;
+
+//     countriesContainer.insertAdjacentHTML('beforeend', html);
+//     countriesContainer.style.opacity = 1;
+//   });
+// };
+
+const renderCountry = function (data, className = '') {
+  const name = data.name.common;
+  const flag = data.flags.svg;
+  const region = data.region;
+  const language = Object.values(data.languages)[0];
+  const currency = Object.values(data.currencies)[0].name;
+  const symbol = Object.values(data.currencies)[0].symbol;
+
+  const html = `    
     <article class="country ${className}">
     <img class="country__img" src="${flag}" alt=${data.flags.alt}/>
     <div class="country__data">
@@ -36,12 +64,41 @@ const getCountryData = function (data, className = '') {
   </article>
 `;
 
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (data) {
+  // Ajax call country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${data}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    //  render country #1
+    renderCountry(data);
+
+    // get neighbour country
+    const neighbour = data.borders?.[0];
+
+    // Ajax call #2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const [data2] = JSON.parse(this.responseText);
+      console.log(data2);
+      renderCountry(data2, 'neighbour');
+    });
   });
 };
 
-getCountryData('portugal');
-getCountryData('usa');
-getCountryData('france');
-getCountryData('germany');
+// getCountryAndNeighbour('portugal');
+getCountryAndNeighbour('usa');
+// getCountryData('usa');
+// getCountryData('france');
+// getCountryData('germany');
