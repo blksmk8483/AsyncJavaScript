d8b43efed9c2f931b3c1777c634a4df6ca418db3
Brandon Knight <knightbrandon1984@gmail.com>
2024-02-16 11:01:02 -0600

    practice using the other promise combinators: race, allSettled, any

--- script.js
+++ script.js
@@ -552,3 +552,48 @@ const get3Countries = async function (c1, c2, c3) {
 };
 
 get3Countries('portugal', 'canada', 'tanzania');
+
+// ==================================================
+// ===========OTHER PROMISE COMBINATORS==============
+// ==================================================
+
+// Promise.race
+(async function () {
+  const res = await Promise.race([
+    getJSON(`https://restcountries.com/v2/name/italy`),
+    getJSON(`https://restcountries.com/v2/name/egypt`),
+    getJSON(`https://restcountries.com/v2/name/mexico`),
+  ]);
+  console.log(res[0]);
+})();
+
+const timeout = function (seconds) {
+  return new Promise(function (response, reject) {
+    setTimeout(function () {
+      reject(new Error('Request took to long!'));
+    }, seconds * 1000);
+  });
+};
+
+Promise.race([
+  getJSON(`https://restcountries.com/v2/name/mexico`),
+  timeout(0.1),
+])
+  .then(res => console.log(res[0]))
+  .catch(err => console.error(err));
+
+// Promise.allSettled
+Promise.allSettled([
+  Promise.resolve('Success'),
+  Promise.reject('Error'),
+  Promise.resolve('Another success!'),
+]).then(res => console.log(res));
+
+// Promise.any
+Promise.any([
+  // Promise.resolve('Success'),
+  Promise.reject('Error'),
+  Promise.resolve('Another success!'),
+])
+  .then(res => console.log(res))
+  .catch(err => console.log(err));
